a1a2abcd3c916b250fd874398e03948cc7dcaa05
8-BIT-DEV <44956905+8-BIT-DEV@users.noreply.github.com>
2019-02-28 13:04:39 -0800

    Version 0.4 Source

---
+++ RedScript Source Ver. 0.4/src/red/core/Function.java
@@ -0,0 +1,18 @@
+package red.core;
+
+public class Function {
+	
+	public String name;
+	public int locate;
+	public int end;
+	public boolean isPublic;
+	
+	public Function(String name, int locate, int end) {
+		
+		this.name = name;
+		this.locate = locate;
+		this.end = end;
+		
+	}
+	
+}
